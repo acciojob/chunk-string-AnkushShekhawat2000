2d8ebd87a04645d2a62939b9828ed5b552399877
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 12:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-// function stringChop(str, size) {
-//   // your code here
-// }
-
-// // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
-// alert(stringChop(str, size));
-
-
-
-
-
-
-
-
-
 function stringChop(str, size) {
   if (str === null) {
     return [];
   }
+
+  size = parseInt(size); // Convert size to a number
 
   const chunks = [];
   let i = 0;
@@ -28,11 +13,12 @@ function stringChop(str, size) {
     i += size;
   }
 
+	
   return chunks;
 }
 
 // Example
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(JSON.stringify(stringChop(str, size)));
 
+alert(JSON.stringify(stringChop(str, size)));
